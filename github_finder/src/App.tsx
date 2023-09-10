6b820bce78a0d89b26bd3ab1b2a93e3480bf48b2
João Damião <thejoaodamiao@gmail.com>
2023-09-10 17:44:49 -0300

    first commit

---
+++ github_finder/src/App.tsx
@@ -0,0 +1,14 @@
+import {Outlet} from "react-router-dom";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Git finder</h1>
+    <Outlet/>
+    </>
+  )
+}
+
+export default App
---
+++ github_finder/src/routes/Home.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import Search from "../components/Search"
+import { UserProps } from "../Types/user"
+
+const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser =async (userName:string) => {
+     const res =await fetch(`https://api.github.com/users/${userName}`);
+
+     const data = await res.json();
+
+     const {avatar_url, login, location, followers, following} = data;
+
+     const userData: UserProps ={
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+     }
+
+    setUser(userData);
+
+    
+  }
+
+  return (
+    <div>
+      <Search loadUser={loadUser}/>
+    </div>
+  )
+}
+
+export default Home;
